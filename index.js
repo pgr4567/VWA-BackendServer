be6415ec0c4eb91e8918298d666be34ed8f0cd3b
pgr4567 <32647406+pgr4567@users.noreply.github.com>
2021-01-06 19:25:14 +0100

    Add /getFriends endpoint.

--- index.js
+++ index.js
@@ -527,6 +527,41 @@ app.get("/removeFriend", function (req, res) {
 	);
 });
 
+app.get("/getFriends", function (req, res) {
+	if (req.query === undefined) {
+		res.send(unexpected_error);
+		return;
+	}
+
+	let username = req.query.username;
+
+	if (username == undefined) {
+		res.send(unexpected_error);
+		return;
+	}
+
+	con.query(
+		"SELECT * FROM players WHERE username = ?",
+		[username],
+		function (err, result) {
+			if (err) {
+				console.log(err);
+				res.send(unexpected_error);
+				return;
+			}
+			if (Object.keys(result).length == 0) {
+				res.send(username_not_exist);
+				return;
+			}
+			Object.keys(result).forEach(function (key) {
+				var row = result[key];
+				res.send(row.friends);
+				return;
+			});
+		}
+	);
+});
+
 app.listen(port, "0.0.0.0", () => {
 	console.log(`BackendServer listening on port ${port}.`);
 });
